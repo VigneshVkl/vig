48f36716b1a821f17b62f1e5e8d12c2c0a5dad88
VigneshVkl <35764577+VigneshVkl@users.noreply.github.com>
2018-02-07 16:25:14 +0530

    Create vignesh2(07).c

---
+++ vignesh2(07).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d=0;
+printf("enter the three digit number");
+scanf("%d",&c);
+b=a;
+while(b==0)
+{
+c=b*a;
+c=c*c;
+}
+printf("enter the three digit value");
+getch();
+return 0;
+ }
+
+
